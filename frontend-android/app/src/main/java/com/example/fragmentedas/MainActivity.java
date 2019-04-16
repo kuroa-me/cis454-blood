712acba3620106947c8b6fc41bc9544dc13ca903
Incfex <kuro@kuroa.me>
2019-04-16 16:46:47 -0400

    frontend: try to fix actionbar

--- frontend-android/app/src/main/java/com/example/fragmentedas/MainActivity.java
+++ frontend-android/app/src/main/java/com/example/fragmentedas/MainActivity.java
@@ -4,15 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.ActionBar;
 
 public class MainActivity extends AppCompatActivity implements select.OnFragmentInteractionListener, register.OnFragmentInteractionListener, login.OnFragmentInteractionListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
+        Toolbar mainTB = findViewById(R.id.my_toolbar);
+        setSupportActionBar(mainTB);
+        ActionBar mainAB = getSupportActionBar();
+
+    }
+
+
+    public boolean onCreatedOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar, menu);
+        return true;
     }
 
     @Override
